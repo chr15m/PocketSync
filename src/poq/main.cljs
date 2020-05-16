10593a6a584825d831328d3c07992ade8511e748
Chris McCormick <chris@mccormick.cx>
2020-05-16 13:41:34 +0800

    Click track playing correctly.

--- src/poq/main.cljs
+++ src/poq/main.cljs
@@ -8,57 +8,106 @@
     [shadow.resource :as rc]))
 
 (defonce state (local-storage (r/atom {}) :state))
+(defonce audio (atom {}))
 
-(defn update-loop! [state]
-  (js/alert "update loop\n" (str @state)))
+(defn add-noise [x]
+  (+ (* (- (js/Math.random) 0.5) 0.0001) x))
 
-(defn editable-wrapper [state props]
-  (let [v (atom (@state :bpm))]
-    (assoc props
-           :on-blur (js/alert "out")
-           :value (or @v 180))))
+(defn make-click-track! [context {:keys [track bpm swing]}]
+  (tap> {"make-click-track!" [track bpm swing]})
+  (let [beat-seconds (/ (/ 60 bpm) 2)
+        beats 16
+        sample-rate (aget context "sampleRate")
+        frames-per-beat (int (* beat-seconds sample-rate))
+        frame-count (* beats frames-per-beat)
+        swing-frames (-> swing (/ 100) (* frames-per-beat))
+        buffer (.createBuffer context 2 frame-count sample-rate)
+        source (.createBufferSource context)]
+    (tap> {"sample-rate" sample-rate})
+    (tap> {"frames-per-beat" frames-per-beat})
+    (doseq [b [0]]
+      (let [array-buffer (.getChannelData buffer b)]
+        (js/console.log (clj->js (range beats)) beat-seconds sample-rate frames-per-beat)
+        (doseq [beat (range beats) i (range frames-per-beat)]
+          (let [odd-beat (boolean (mod beat 2))])
+          ;(js/console.log (< i 882))
+          (aset array-buffer
+                (+ i (* beat frames-per-beat))
+                (if
+                  (if (= (mod beat 2) 0)
+                    (< i 882)
+                    (and (< (- i swing-frames) 882) (> i swing-frames)))
+                  (add-noise -1)
+                  (add-noise 1))))
+        (js/console.log array-buffer)))
+    (aset source "loop" true)
+    (aset source "buffer" buffer)
+    (.connect source (aget context "destination"))
+    (.start source)
+    (when track
+      (tap> {"Stopping old track." track})
+      (.stop track))
+    (tap> "Done generating.")
+    source))
+
+(defn update-loop! [state ev]
+  (when (@state :playing)
+    (swap! state #(-> % (assoc :track (make-click-track! (@audio :context) %))))))
+
+(defn play! [state ev]
+  (swap! state assoc :playing true)
+  (update-loop! state ev))
+
+(defn stop! [state ev]
+  (swap! state dissoc :playing)
+  (let [track (@state :track)]
+    (when track
+      (.stop track))))
 
 (defn update-val! [state k ev]
   (swap! state
-         assoc k (-> ev .-target .-value)))
+         assoc k (int (-> ev .-target .-value))))
 
 (defn component-main [state]
   (let [bpm (-> (or (@state :bpm) 180) int (min 200) (max 0))
-        swing (-> (or (@state :swing) 0) int (min 100) (max 0))]
-  [:div
-   [:div.input-group
-    [:label
-     [:input
-      {:type "range"
-       :min 30
-       :max 200
-       :on-change (partial update-val! state :bpm)
-       :on-mouse-up (partial update-loop! state)
-       :on-touch-end (partial update-loop! state)
-       :value bpm}]
-     "tempo"]]
-   [:div.input-group
-    [:input#bpm
-     {:type "number"
-      :on-change (partial update-val! state :bpm)
-      :on-blur (partial update-loop! state)
-      :on-key-up #(if (= (.-keyCode %) 13) (-> % .-target .blur))
-      :on-input #(when (not= (aget js/document "activeElement") (.-target %))
-                  (update-loop! state))
-      :value bpm}]]
-   [:div.input-group
-    [:label
-     [:input {:type "range"
-              :min 0
-              :max 100
-              :on-change (partial update-val! state :swing)
-              :on-mouse-up (partial update-loop! state)
-              :on-touch-end (partial update-loop! state)
-              :value swing}]
-     "swing"]]
-   [:div.input-group
-    [:button "play"]
-    [:button "tap"]]]))
+        swing (-> (or (@state :swing) 0) int (min 100) (max 0))
+        playing (@state :playing)]
+    [:div
+     [:div.input-group
+      [:label
+       [:input
+        {:type "range"
+         :min 30
+         :max 200
+         :on-change (partial update-val! state :bpm)
+         :on-mouse-up (partial update-loop! state)
+         :on-touch-end (partial update-loop! state)
+         :value bpm}]
+       "tempo"]]
+     [:div.input-group
+      [:input#bpm
+       {:type "number"
+        :on-change (partial update-val! state :bpm)
+        :on-blur (partial update-loop! state)
+        :on-key-up #(if (= (.-keyCode %) 13) (-> % .-target .blur))
+        :on-input #(when (not= (aget js/document "activeElement") (.-target %))
+                     (update-loop! state nil))
+        :value bpm}]]
+     (comment [:div.input-group
+               [:label
+                [:input {:type "range"
+                         :min 0
+                         :max 100
+                         :on-change (partial update-val! state :swing)
+                         :on-mouse-up (partial update-loop! state)
+                         :on-touch-end (partial update-loop! state)
+                         :value swing}]
+                "swing"]])
+     [:div.input-group
+      (if playing
+        [:button {:on-click (partial stop! state)} "stop"]
+        [:button {:on-click (partial play! state)} "play"])
+      [:button "tap"]]]))
 
 (defn reload! []
   (rdom/render [component-main state] (js/document.getElementById "app")))
@@ -66,5 +115,7 @@
 (defn main! []
   (go
     (<! (timeout 1000))
-    (swap! state assoc :audio-context (js/AudioContext.))
+    (swap! state dissoc :playing)
+    (let [audio-context (or (aget js/window "AudioContext") (aget js/window "webkitAudioContext"))]
+      (swap! audio assoc :context (audio-context.)))
     (reload!)))
