b88fc3aad7cb33ba6e14e05993ab34518b83644b
Chris McCormick <chris@mccormick.cx>
2023-01-04 16:26:01 +0800

    Design updates. Testing rotary BPM dial. #5
    Fixes #7.

--- src/poq/main.cljs
+++ src/poq/main.cljs
@@ -12,6 +12,9 @@
                     :audio-source nil
                     :context nil})
 
+(def bpm-min 60)
+(def bpm-max 240)
+
 (def local-storage-keys [:bpm :swing])
 
 (defonce state (local-storage (r/atom initial-state)
@@ -49,17 +52,17 @@
   (assoc *state :audio-source
          (seamless-loop-audio-buffer! context audio-buffer audio-source)))
 
-(defn update-loop! [state _ev]
+(defn update-loop! [state]
   (let [{:keys [playing]} @state]
     (when playing
       (swap! state
              #(-> % make-click-track-audio-buffer play-click-track!)))))
 
-(defn play! [state ev]
+(defn play! [state]
   (swap! state assoc :playing true)
-  (update-loop! state ev))
+  (update-loop! state))
 
-(defn stop! [state _ev]
+(defn stop! [state]
   (let [click-track-audio-source (@state :audio-source)]
     (swap! state dissoc :playing :audio-source :audio-buffer)
     (stop-source! click-track-audio-source)))
@@ -89,19 +92,47 @@
               bpm)]
     (assoc *state :bpm bpm :taps taps)))
 
-(defn tap! [state ev]
+(defn tap! [state]
   (let [previous-state @state
         updated-state (swap! state new-tap)]
     (when (not= updated-state previous-state)
       (js/console.log "CHANGED")
-      (update-loop! state ev))))
+      (update-loop! state))))
 
 (defn update-val! [state k ev]
   (swap! state
          assoc-in k (int (-> ev .-target .-value))))
 
+(defn bpm-to-degrees [bpm]
+  (let [normalized (-> bpm (- bpm-min) (/ (- bpm-max bpm-min)))]
+    (-> normalized
+      (* 350)
+      (- 85))))
+
+(defn get-bpm [*state]
+  (-> *state :bpm int (min bpm-max) (max bpm-min)))
+
+(defn component-bpm-dial [state]
+  (let [touchstate (atom nil)]
+    (fn []
+      (let [bpm (get-bpm @state)]
+        [:svg#tempo {:viewBox "-100 -100 200 200"
+                     :on-mouse-down #(reset! touchstate true)
+                     :on-mouse-out #(reset! touchstate false)
+                     :on-mouse-up #(reset! touchstate false)
+                     :on-mouse-move #(when @touchstate (js/console.log %))}
+         [:text {:x 0 :y -40 :text-anchor "middle" :dominant-baseline "middle"} bpm]
+         [:g {:transform (str "rotate(" (bpm-to-degrees bpm) " 0 0)")}
+          [:g {:transform "translate(85 0)"}
+           [:circle {:cx 0 :cy 0 :r 10 :stroke-width "2px"}]]]
+         [:circle {:cx 0 :cy 0 :r 85 :fill "transparent" :path-length 5}]
+         (for [b (range bpm-min (inc bpm-max) 10)]
+           [:g {:transform (str "rotate(" (bpm-to-degrees b) " 0 0)")
+                :key (str "tick-" b)}
+            [:line {:x1 85 :y1 0 :x2 90 :y2 0}]])]))))
+
 (defn component-main [state]
-  (let [bpm (-> @state :bpm int (min 240) (max 60))
+  (let [bpm (get-bpm @state)
         swing (-> @state :swing int (min 100) (max 0))
         playing (@state :playing)]
     [:div
@@ -109,37 +140,25 @@
       [:label
        [:input
         {:type "range"
-         :min 60
-         :max 240
+         :min bpm-min
+         :max bpm-max
          :on-change (partial update-val! state [:bpm])
-         :on-mouse-up (partial update-loop! state)
-         :on-touch-end (partial update-loop! state)
-         :value bpm}]
-       "tempo"]]
-     [:div.input-group
-      [:input#bpm
-       {:type "number"
-        :on-change (partial update-val! state [:bpm])
-        :on-blur (partial update-loop! state)
-        :on-key-up #(when (= (.-keyCode %) 13) (-> % .-target .blur))
-        :on-input #(when (not= (aget js/document "activeElement") (.-target %))
-                     (update-loop! state nil))
-        :value bpm}]]
+         :on-mouse-up #(update-loop! state)
+         :on-touch-end #(update-loop! state)
+         :value bpm}]]]
+     [component-bpm-dial state]
+     [:button#tap {:on-click #(tap! state)} "tap"]
      [:div.input-group
       [:label
        [:input {:type "range"
                 :min 0
                 :max 75
                 :on-change (partial update-val! state [:swing])
-                :on-mouse-up (partial update-loop! state)
-                :on-touch-end (partial update-loop! state)
-                :value swing}]
-       "swing"]]
+                :on-mouse-up #(update-loop! state)
+                :on-touch-end #(update-loop! state)
+                :value swing}]]]
      [:div.input-group
-      (if playing
-        [:button {:on-click (partial stop! state)} "stop"]
-        [:button {:on-click (partial play! state)} "play"])
-      [:button {:on-click (partial tap! state)} "tap"]]]))
+      [:button {:on-click #(if playing (stop! state) (play! state))} "stop"]]]))
 
 (defn reload! {:dev/after-load true} []
   (rdom/render [component-main state] (js/document.getElementById "app")))
